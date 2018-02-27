7f380b8b1342ef546923bb5d87ce976f585b2b0a
ke260g <ke260g@outlook.com>
2018-02-27 13:58:38 +0800

    Remove codes for debug.

--- main.cpp
+++ main.cpp
@@ -1,27 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-#include<QDir>
-#include<QDebug>
-
-#include <QStyle>
-#include <QDesktopWidget>
-
 int main(int argc, char *argv[])
 {
-    QCoreApplication::addLibraryPath("./");
     QApplication a(argc, argv);
     MainWindow w;
 
-#if 0 // set window in the center of screen
-    // a bug that hidden the title of MainWindow
-    w.setGeometry(QStyle::alignedRect(
-                           Qt::LayoutDirectionAuto,
-                           Qt::AlignCenter,
-                           w.size(),
-                           a.desktop()->availableGeometry()));
-#endif
-    w.setWindowTitle("PathEditor");
     w.show();
     w.showAfter();
 
