04bbeb0a56a34e5ea13c10892c58476cb488908a
ke260g <ke260g@gmail.com>
2018-02-27 16:21:41 +0800

    Add win32 self evelate method.

---
+++ win32uac.cpp
@@ -0,0 +1,140 @@
+#include "win32uac.h"
+
+#include <windows.h>
+#include <windowsx.h>
+#include <strsafe.h>
+#include <shlobj.h>
+
+#pragma comment(lib, "advapi32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "winspool.lib")
+#pragma comment(lib, "comdlg32.lib")
+#pragma comment(lib, "advapi32.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "oleaut32.lib")
+#pragma comment(lib, "uuid.lib")
+#pragma comment(lib, "odbc32.lib")
+#pragma comment(lib, "odbccp32.lib")
+
+
+#include <QDebug>
+
+//
+//   FUNCTION: IsRunAsAdmin()
+//
+//   PURPOSE: The function checks whether the current process is run as
+//   administrator. In other words, it dictates whether the primary access
+//   token of the process belongs to user account that is a member of the
+//   local Administrators group and it is elevated.
+//
+//   RETURN VALUE: Returns TRUE if the primary access token of the process
+//   belongs to user account that is a member of the local Administrators
+//   group and it is elevated. Returns FALSE if the token does not.
+//
+//   EXCEPTION: If this function fails, it throws a C++ DWORD exception which
+//   contains the Win32 error code of the failure.
+//
+//   EXAMPLE CALL:
+//     try
+//     {
+//         if (IsRunAsAdmin())
+//             wprintf (L"Process is run as administrator\n");
+//         else
+//             wprintf (L"Process is not run as administrator\n");
+//     }
+//     catch (DWORD dwError)
+//     {
+//         wprintf(L"IsRunAsAdmin failed w/err %lu\n", dwError);
+//     }
+//
+bool WIN32UAC::isRunAsAdmin()
+{
+    BOOL fIsRunAsAdmin = FALSE;
+    DWORD dwError = ERROR_SUCCESS;
+    PSID pAdministratorsGroup = NULL;
+
+    // Allocate and initialize a SID of the administrators group.
+    SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+    if (!AllocateAndInitializeSid(
+                &NtAuthority,
+                2,
+                SECURITY_BUILTIN_DOMAIN_RID,
+                DOMAIN_ALIAS_RID_ADMINS,
+                0, 0, 0, 0, 0, 0,
+                &pAdministratorsGroup))
+    {
+        dwError = GetLastError();
+        goto Cleanup;
+    }
+
+    // Determine whether the SID of administrators group is enabled in
+    // the primary access token of the process.
+    if (!CheckTokenMembership(NULL, pAdministratorsGroup, &fIsRunAsAdmin))
+    {
+        dwError = GetLastError();
+        goto Cleanup;
+    }
+
+Cleanup:
+    // Centralized cleanup for all allocated resources.
+    if (pAdministratorsGroup)
+    {
+        FreeSid(pAdministratorsGroup);
+        pAdministratorsGroup = NULL;
+    }
+
+    // Throw the error if something failed in the function.
+    if (ERROR_SUCCESS != dwError)
+    {
+        throw dwError;
+    }
+
+    return fIsRunAsAdmin;
+}
+
+
+bool WIN32UAC::elevate() {
+    // Check the current process's "run as administrator" status.
+    BOOL fIsRunAsAdmin;
+    try
+    {
+        fIsRunAsAdmin = WIN32UAC::isRunAsAdmin();
+    }
+    catch (DWORD dwError)
+    {
+        qDebug() << "Error: isRunAsAdmin() " << dwError;
+        return false;
+    }
+
+    // Elevate the process if it is not run as administrator.
+    if (!fIsRunAsAdmin)
+    {
+        wchar_t szPath[MAX_PATH];
+        if (GetModuleFileName(NULL, szPath, ARRAYSIZE(szPath)))
+        {
+            // Launch itself as administrator.
+            SHELLEXECUTEINFO sei = { sizeof(sei) };
+            sei.lpVerb = L"runas";
+            sei.lpFile = szPath;
+            sei.hwnd = GetActiveWindow();
+            sei.nShow = SW_NORMAL;
+
+            if (!ShellExecuteEx(&sei))
+            {
+                DWORD dwError = GetLastError();
+                if (dwError == ERROR_CANCELLED)
+                {
+                    qDebug() << "elevate: ERROR_CANCELLED";
+                }
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+    return true;
+}
---
+++ win32uac.h
@@ -0,0 +1,17 @@
+#ifndef WIN32UAC_H
+#define WIN32UAC_H
+
+// some method for user access control in win32:
+// method:
+//  1. selfELevation, to kacquire Administrative Privileges
+class WIN32UAC {
+private:
+    WIN32UAC();
+    ~WIN32UAC();
+public:
+    static void f();
+    static bool isRunAsAdmin();
+    static bool elevate();
+};
+void g();
+#endif // WIN32UAC_H
